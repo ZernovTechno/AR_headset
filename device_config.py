c841fa8b69568b801ee4ec8648327120f59becc3
usiqwerty <benlogin1@protonmail.com>
2024-04-14 19:52:09 +0500

    теперь точно

--- device_config.py
+++ device_config.py
@@ -1,4 +1,4 @@
-camera_size = (1280, 720)
-screen_size = camera_size#(1920, 1080)
+camera_size = (720*2, 720)
+screen_size = camera_size #(1920, 1080)
 
 eye_size = (screen_size[0]//2, screen_size[1])
--- eyes.py
+++ eyes.py
@@ -16,35 +16,32 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 class Eye:
     prev_frame_time = 0
     new_frame_time = 0
-    frame: np.ndarray = None
     name: str
 
     def __init__(self, name, camera):
         self.name = name
         self.camera = camera
 
-    def put_frame(self, frame: np.ndarray):
-        self.frame = frame
-
-
     def job(self):
         print(f"Eye job started: {self.name}")
+        frame = np.zeros([eye_width, eye_height, 3], dtype=np.uint8)
         while True:
             if self.name == 'left':
                 frame = cv2.resize(self.camera.frame[:camera_height, 0:camera_width // 2], eye_size)
             else:
-                frame = cv2.resize(self.camera.frame[:camera_height, (camera_width // 2):camera_width], eye_size)
-
+                try:
+                    frame = cv2.resize(self.camera.frame[:camera_height, (camera_width // 2): camera_width], eye_size)
+                except:
+                    pass
             new_frame_time = time.time()
             fps = 1 / (new_frame_time - self.prev_frame_time)
             self.prev_frame_time = new_frame_time
             fps = str(int(fps))
-            cv2.rectangle(self.frame, (7, 0), (7 + 300, 70), (0, 0, 0), -1)
+            # cv2.rectangle(self.frame, (7, 0), (7 + 300, 70), (0, 0, 0), -1)
             cv2.putText(frame, fps, (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
 
             cv2.imshow(self.name, frame)
             if cv2.waitKey(imshow_delay) & 0xFF == ord('q'):
                 exit(0)
-
 
             # cv2.putText(self.frame, fps, (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
