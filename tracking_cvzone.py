9efe672e3ddcb8b6818e77abbd7b5d943b8025b6
Zernov <90546939+ZernovTechno@users.noreply.github.com>
2024-04-22 14:11:23 +0500

    Update tracking_cvzone.py

--- tracking_cvzone.py
+++ tracking_cvzone.py
@@ -8,6 +8,21 @@ hand_detector = HandDetector()
 
 class controller():
 
+    def remove_background(self,img):
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        
+        ret, mask = cv2.threshold(gray, 0, 255, cv2.THRESH_TRIANGLE) 
+        mask = cv2.bitwise_not(mask)
+        kernel = np.ones((9,9), np.uint8)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
+        result = img.copy()
+        result = cv2.cvtColor(result, cv2.COLOR_BGR2BGRA)
+        result[:, :, 3] = mask
+
+        return result, mask
+
     def find_and_get_hands(self, img):
         hands, frame = hand_detector.findHands(img, draw=False, flipType=False)
         if hands:
