d5b448fb9a6006b9e954141c87986c73ba98ad1a
Zernov <90546939+ZernovTechno@users.noreply.github.com>
2024-04-30 23:01:43 +0500

    Update tracking_mp_opt.py
    Update mp

--- tracking_mp_opt.py
+++ tracking_mp_opt.py
@@ -13,52 +13,46 @@ mp_draw = mp.solutions.drawing_utils
 segmentor = SelfiSegmentation()
 class controller():
     def remove_background(self,img):
-        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        
-        ret, mask = cv2.threshold(gray, 0, 255, cv2.THRESH_TRIANGLE) 
-        mask = cv2.bitwise_not(mask)
-        kernel = np.ones((9,9), np.uint8)
-        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+        try:
+            gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    
+            _, mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU) 
+            mask = cv2.bitwise_not(mask)
+            mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, np.ones((9, 9), np.uint8))
 
-        result = img.copy()
-        result = cv2.cvtColor(result, cv2.COLOR_BGR2BGRA)
-        result[:, :, 3] = mask
+            result = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
+            result[:, :, 3] = mask
 
-        return result, mask
+            return result, mask
+        except:
+            return img, img
+            pass
     
     def find_and_get_hands(self, image):
         results = hands.process(image)
 
         lmList = []
-        miny = 0
-        minx = 0
-        maxy = 0
-        maxx = 0
+        miny, minx, maxy, maxx = 0, 0, 0, 0
         mask = 0
+
         if results.multi_hand_landmarks:
             myHand = results.multi_hand_landmarks[0]
             for id, lm in enumerate(myHand.landmark):
                 h, w, c = image.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 lmList.append([id, cx, cy])
-        if (len(lmList) >= 20):
-            xlist = []
-            ylist = []
 
-            for ids in lmList:
-                xlist.append(ids[1])
-                ylist.append(ids[2])
+        if len(lmList) >= 20:
+            lmArray = np.array(lmList)
+            xlist = lmArray[:, 1]
+            ylist = lmArray[:, 2]
 
-            minx = min(xlist) - 40
-            maxx = max(xlist) + 40
-            miny = min(ylist) - 40
-            maxy = max(ylist) + 40
-            if (minx <= 0): minx = 1
-            if (maxx <= 0): maxx = 1
-            if (maxy <= 0): maxy = 1
-            if (miny <= 0): miny = 1
+            minx = np.clip(np.min(xlist) - 40, 1, None)
+            maxx = np.clip(np.max(xlist) + 40, 1, None)
+            miny = np.clip(np.min(ylist) - 40, 1, None)
+            maxy = np.clip(np.max(ylist) + 40, 1, None)
 
             image = image[miny:maxy, minx:maxx]
-            image, mask = self.remove_background(image)    
+            image, mask = self.remove_background(image)
+
         return image, lmList, miny, minx, maxy, maxx, mask
