56ea19f7d65ed1206dcb65e680ef7fc3c5d084c8
Zernov <90546939+ZernovTechno@users.noreply.github.com>
2024-05-12 17:13:40 +0500

    Update tracking_cvzone.py
    cvzone fix

--- tracking_cvzone.py
+++ tracking_cvzone.py
@@ -8,23 +8,13 @@ hand_detector = HandDetector()
 
 class controller():
 
-    def remove_background(self,img):
-        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        
-        ret, mask = cv2.threshold(gray, 0, 255, cv2.THRESH_TRIANGLE) 
-        mask = cv2.bitwise_not(mask)
-        kernel = np.ones((9,9), np.uint8)
-        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-
-        result = img.copy()
-        result = cv2.cvtColor(result, cv2.COLOR_BGR2BGRA)
-        result[:, :, 3] = mask
-
-        return result, mask
-
     def find_and_get_hands(self, img):
         hands, frame = hand_detector.findHands(img, draw=False, flipType=False)
+        min_x = 0
+        max_x = 0
+        min_y = 0
+        max_y = 0
+        landmark = [0]
         if hands:
             hand1 = hands[0]  # Get the first hand detected
             landmark = hand1["lmList"]  # List of 21 landmarks for the first hand
@@ -42,4 +32,4 @@ class controller():
             #src = segmentor.removeBG(crop_img, (0, 0, 0), 0.3)
             _, alpha = cv2.threshold(cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY)
             frame[min_y:max_y, min_x:max_x][alpha != 0] = crop_img[alpha != 0]
-        return frame
+        return frame, landmark, min_y, min_x, max_y, max_x, frame
